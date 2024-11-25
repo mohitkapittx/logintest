d9108cbd07758c01aae06f9e35f0eb9bfec33d85
Akshay Bhad <akshay.automation95@gmail.com>
2024-11-25 17:45:25 +0530

    add comment

--- src/test/java/Runnerclass/run.java
+++ src/test/java/Runnerclass/run.java
@@ -14,6 +14,7 @@ import io.cucumber.junit.CucumberOptions;
 		dryRun = false,//to check the mapping is proper between feature file and step def file
 		tags ="@logtest")	
 
+// mohit
 public class run {
 
 }
